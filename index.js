7795bb562f97120866dc90f9284f3358532bbd99
root <aashishkunwar1201@gmail.com>
2026-06-07 17:45:29 +0545

    Updated message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 8000;
 
 app.use('/',(req,res)=>{
-	res.json({"message":"Hello World"});
+	res.json({"message":"Updated Hello World"});
 })
 
 app.listen(PORT, () => console.log(`Server listening on PORT: ${PORT}`))
